0e80aae3a3b613bd5aa27b4d3abd2d0cdb6292d4
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-02-01 09:36:30 -0800

    Create month grid

--- src/Calendar/components/CalendarViews/CalendarMonthView/utils/getDayGrid.js
+++ src/Calendar/components/CalendarViews/CalendarMonthView/utils/getDayGrid.js
@@ -1,3 +1,87 @@
-const getDayGrid = () => {};
+// importing moment for JSDOC only
+// eslint-disable-next-line no-unused-vars
+import moment from "moment";
+
+/**
+ * Get the start of the array grid. Last months dates, something like: [28, 29, 30, 31]
+ *
+ * @param {Object} params
+ * @param {moment} params.date - the selected date we want the month grid for
+ * @param {0|1|2|3|4|5|6} params.firstDay - the day of the week the month starts on 0 = Sunday
+ * @returns {number[]}
+ */
+const getStartOfGrid = ({ date, firstDay }) => {
+  const monthStartPadding =
+    date
+      .clone()
+      .startOf("month")
+      .day() - firstDay;
+
+  const lastDayOfLastMonth = date
+    .clone()
+    .subtract(1, "months")
+    .endOf("month")
+    .date();
+
+  const startOfGrid = [];
+  for (
+    let i = lastDayOfLastMonth - monthStartPadding + 1;
+    i <= lastDayOfLastMonth;
+    i += 1
+  ) {
+    startOfGrid.push(i);
+  }
+
+  return startOfGrid;
+};
+
+/**
+ * Get an array like [1, 2, 3, ...] for each day of the month
+ *
+ * @param {moment} date - the date we want an array of days of the month for
+ * @returns {number[]}
+ */
+export const getArrayOfDays = date => {
+  const totalDays = date.daysInMonth();
+  const days = [];
+  for (let i = 1; i <= totalDays; i += 1) {
+    days.push(i);
+  }
+  return days;
+};
+
+/**
+ * Get the first days of the next month depending on how many days are left
+ *
+ * @param {Object} params
+ * @param {moment} params.date - the selected date
+ * @param {number} params.totalDates - the total amount of dates we have so far
+ */
+const getEndOfGrid = totalDates => {
+  // A 7x6 grid (7 days a week and 6 potential weeks) has 42 days we need to know
+  const totalGridSize = 42;
+  const remainingDates = totalGridSize - totalDates;
+  const days = [];
+  for (let i = 1; i <= remainingDates; i += 1) {
+    days.push(i);
+  }
+  return days;
+};
+
+/**
+ * Get an array of the grid for a month based on a given first day and date
+ *
+ * @param {Object} params
+ * @param {moment} params.date - the selected date we want the month grid for
+ * @param {0|1|2|3|4|5|6} params.firstDay - the day of the week the month starts on 0 = Sunday
+ * @returns {number[]}
+ */
+const getDayGrid = ({ date, firstDay = 0 }) => {
+  const startOfGrid = getStartOfGrid({ date, firstDay });
+  const middleOfGrid = getArrayOfDays(date);
+  const endOfGrid = getEndOfGrid(startOfGrid.length + middleOfGrid.length);
+
+  return [].concat(startOfGrid, middleOfGrid, endOfGrid);
+};
 
 export default getDayGrid;
--- src/Calendar/components/CalendarViews/CalendarMonthView/utils/getDayGrid.test.js
+++ src/Calendar/components/CalendarViews/CalendarMonthView/utils/getDayGrid.test.js
@@ -1,10 +1,91 @@
-// import getDayGrid from "./getDayGrid";
+import getDayGrid, { getArrayOfDays } from "./getDayGrid";
+import isEqual from "lodash/isEqual";
+import moment from "moment";
+
+const getTestInfo = ({ dateString, firstDay }) => {
+  const date = moment(dateString);
+  const arrayOfDays = getArrayOfDays(date);
+  const result = getDayGrid({ date, firstDay });
+  return {
+    result,
+    arrayOfDays
+  };
+};
+
 describe("The monthly grid function", () => {
+  it("should have 42 days", () => {
+    const { result } = getTestInfo({
+      dateString: "2019-01-15",
+      firstDay: 0
+    });
+    expect(result.length === 42).toBeTruthy();
+  });
+
   it("should have the correct grid for Sunday as the first day", () => {
-    // const result = getDayGrid({ date: new Date("2019-01-15"), firstDay: 0 });
-    // const expected = [
-        
-    // ];
-    expect(true).toBeTruthy();
+    const { result, arrayOfDays } = getTestInfo({
+      dateString: "2019-01-15",
+      firstDay: 0
+    });
+    const expected = [30, 31, ...arrayOfDays, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    expect(isEqual(result, expected)).toBeTruthy();
+  });
+
+  it("should have the correct grid for Monday as the first day", () => {
+    const { result, arrayOfDays } = getTestInfo({
+      dateString: "2019-02-15",
+      firstDay: 1
+    });
+    const expected = [
+      28,
+      29,
+      30,
+      31,
+      ...arrayOfDays,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10
+    ];
+    expect(isEqual(result, expected)).toBeTruthy();
+  });
+
+  it("should have the correct grid for Friday as the first day", () => {
+    const { result, arrayOfDays } = getTestInfo({
+      dateString: "2019-03-15",
+      firstDay: 5
+    });
+    const expected = [...arrayOfDays, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    expect(isEqual(result, expected)).toBeTruthy();
+  });
+
+  it("should have the correct grid for Sunday as the first day on leap year", () => {
+    const { result, arrayOfDays } = getTestInfo({
+      dateString: "2020-02-15",
+      firstDay: 0
+    });
+    const expected = [
+      26,
+      27,
+      28,
+      29,
+      30,
+      31,
+      ...arrayOfDays,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7
+    ];
+
+    expect(isEqual(result, expected)).toBeTruthy();
   });
 });

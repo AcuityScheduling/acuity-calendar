544e221f64b1a1e3f446949fbedb16dee4293666
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-02-01 11:44:22 -0800

    Some basic styling

--- src/Calendar/components/CalendarViews/CalendarMonthView/components/CalendarMonthCell.js
+++ src/Calendar/components/CalendarViews/CalendarMonthView/components/CalendarMonthCell.js
@@ -1,9 +1,14 @@
 import React from "react";
+import styles from "./CalendarMonthCell.module.css";
 import CalendarEvent from "../../../CalendarEvent";
+
+const style = {
+  width: `${100 / 7}%`
+};
 
 const CalendarMonthCell = ({ date, events }) => {
   return (
-    <div>
+    <div className={styles.cell} style={style}>
       <h2>{date}</h2>
       {events.length > 0 &&
         events.map(event => <CalendarEvent event={event} />)}
--- src/Calendar/components/CalendarViews/CalendarMonthView/index.js
+++ src/Calendar/components/CalendarViews/CalendarMonthView/index.js
@@ -1,15 +1,35 @@
 import React from "react";
+import styles from "./index.module.css";
 import { getMonthGrid } from "./utils";
 import CalendarMonthCell from "./components/CalendarMonthCell";
 
 const CalendarMonthView = ({ selectedDate }) => {
   const monthGrid = getMonthGrid({ date: selectedDate });
 
-  return monthGrid.map(column => {
-    return column.map(day => {
-      return <CalendarMonthCell date={day} events={[]} />;
-    });
-  });
+  let countDays = 0;
+  let countColumns = 0;
+  return (
+    <div className={styles.wrapper}>
+      {monthGrid.map(column => {
+        countColumns += 1;
+
+        return (
+          <div className={styles.column} key={`monthColumn${countColumns}`}>
+            {column.map(day => {
+              countDays += 1;
+              return (
+                <CalendarMonthCell
+                  date={day}
+                  events={[]}
+                  key={`monthCell${countDays}`}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CalendarMonthView;

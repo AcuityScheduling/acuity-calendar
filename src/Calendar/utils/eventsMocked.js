0b232e3adb5dd739b985240399f4721b290e52f4
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-02-04 14:38:35 -0800

    Update mocked events to use browser timezone

--- src/Calendar/utils/eventsMocked.js
+++ src/Calendar/utils/eventsMocked.js
@@ -2,32 +2,32 @@ import moment from "moment";
 
 export const event1 = {
   id: 1,
-  start: moment("2019-01-02T12:00:00+00:00"),
-  end: moment("2019-01-02T13:00:00+00:00"),
+  start: moment(new Date("2019-01-02 12:00:00")),
+  end: moment(new Date("2019-01-02 12:30:00")),
   title: "Jan 2nd at 12pm",
   allDay: false
 };
 
 export const event2 = {
   id: 2,
-  start: moment("2019-01-02T19:00:00+00:00"),
-  end: moment("2019-01-02T19:25:25+00:00"),
+  start: moment(new Date("2019-01-02 19:00:00")),
+  end: moment(new Date("2019-01-02 19:25:25")),
   title: "Jan 2nd at 7pm",
   allDay: false
 };
 
 export const event3 = {
   id: 3,
-  start: moment("2019-02-01T09:00:00+00:00"),
-  end: moment("2019-02-01T09:30:00+00:00"),
+  start: moment(new Date("2019-02-01 09:00:00")),
+  end: moment(new Date("2019-02-01 09:30:00")),
   title: "Feb 1st at 9am",
   allDay: false
 };
 
 export const event4 = {
   id: 4,
-  start: moment("2019-03-07T14:00:00+00:00"),
-  end: moment("2019-03-07T16:00:00+00:00"),
+  start: moment(new Date("2019-03-07 14:00:00")),
+  end: moment(new Date("2019-03-07 15:00:00")),
   title: "March 7th at 2pm",
   allDay: false
 };

f114ebc1aaf64e2c00d5a973ce1f8ca5a4c9639b
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-02-04 20:16:22 -0800

    Documenting constants

--- src/Calendar/components/CalendarViews/CalendarDaysView/constants/stepHeights.js
+++ src/Calendar/components/CalendarViews/CalendarDaysView/constants/stepHeights.js
@@ -1,3 +1,8 @@
+/**
+ * The height of steps at each "stepMinutes" range in pixels. The total height of the
+ * block gets slightly bigger at each step. 5 is minutes per step, 10 is 10 minutes per
+ * step etc. Prolly overengineered as we prolly won't use more than 30
+ */
 const STEP_HEIGHTS = {
   5: 22,
   10: 27,

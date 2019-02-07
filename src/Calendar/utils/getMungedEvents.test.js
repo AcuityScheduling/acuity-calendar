5aa08a95070f82b28b684c7302082c1ed286ebe7
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-02-07 11:14:27 -0800

    Update test for munged events

--- src/Calendar/utils/getMungedEvents.test.js
+++ src/Calendar/utils/getMungedEvents.test.js
@@ -2,15 +2,45 @@ import getMungedEvents from "./getMungedEvents";
 import { eventsMocked } from "../utils";
 
 describe("Munged Event Object", () => {
+  it("has keys that are calendar ids", () => {
+    const mungedEvents = getMungedEvents({
+      events: eventsMocked,
+      stepMinutes: 30
+    });
+
+    expect(mungedEvents).toHaveProperty("5");
+    expect(mungedEvents).toHaveProperty("6");
+  });
+
   it("has keys that are dates", () => {
     const mungedEvents = getMungedEvents({
       events: eventsMocked,
       stepMinutes: 30
     });
 
-    expect(mungedEvents).toHaveProperty("2019-01-02");
-    expect(mungedEvents).toHaveProperty("2019-02-07");
-    expect(mungedEvents).toHaveProperty("2019-03-07");
+    const calendar5 = mungedEvents[5];
+    const calendar6 = mungedEvents[6];
+
+    expect(calendar5).toHaveProperty("2019-01-02");
+    expect(calendar6).toHaveProperty("2019-02-07");
+    expect(calendar6).toHaveProperty("2019-03-07");
+  });
+
+  it("has keys for columns", () => {
+    const mungedEvents = getMungedEvents({
+      events: eventsMocked,
+      stepMinutes: 30
+    });
+
+    const firstDate = mungedEvents[5]["2019-01-02"];
+    const secondDate = mungedEvents[6]["2019-02-07"];
+    const thirdDate = mungedEvents[6]["2019-03-07"];
+
+    expect(firstDate).toHaveProperty("column_1");
+    expect(secondDate).toHaveProperty("column_1");
+    expect(secondDate).toHaveProperty("column_2");
+    expect(secondDate).toHaveProperty("column_3");
+    expect(thirdDate).toHaveProperty("column_1");
   });
 
   it("has new keys needed for calendar", () => {
@@ -19,27 +49,9 @@ describe("Munged Event Object", () => {
       stepMinutes: 30
     });
 
-    const event = mungedEvents["2019-01-02"][0];
+    const event = mungedEvents[5]["2019-01-02"]["column_1"][0];
 
     expect(event).toHaveProperty("top");
     expect(event).toHaveProperty("height");
-    expect(event).toHaveProperty("column");
-  });
-
-  it("increases columns when it's supposed", () => {
-    const mungedEvents = getMungedEvents({
-      events: eventsMocked,
-      stepMinutes: 30
-    });
-
-    const event1 = mungedEvents["2019-02-07"][0];
-    const event2 = mungedEvents["2019-02-07"][1];
-    const event3 = mungedEvents["2019-02-07"][2];
-    const event4 = mungedEvents["2019-02-07"][3];
-
-    expect(event1.column).toEqual(1);
-    expect(event2.column).toEqual(2);
-    expect(event3.column).toEqual(3);
-    expect(event4.column).toEqual(1);
   });
 });

33f45659d67ecf27f11cd9d46833110fd332a0dc
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-02-07 15:25:02 -0800

    Fix no columns function

--- src/Calendar/utils/getEventsWithoutColumns.js
+++ src/Calendar/utils/getEventsWithoutColumns.js
@@ -2,12 +2,12 @@
  * Events come in keyed by column for day/week view. We should be able to remove those
  * columns. This will remove the columns when necessary
  *
- * @param {array} eventsByColumn - these events must be with a column as a key
+ * @param {array} eventsByColumn - these events must be WITHOUT the calendar id
  */
 const getEventsWithoutColumns = eventsByColumn => {
   return Object.keys(eventsByColumn).reduce((eventsNoColumns, columnKey) => {
-    return Object.assign(eventsNoColumns, eventsByColumn[columnKey]);
-  }, {});
+    return [...eventsNoColumns, ...eventsByColumn[columnKey]];
+  }, []);
 };
 
 export default getEventsWithoutColumns;

cb46642b12b7d01f064224581445d702250474e5
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-02-13 15:28:05 -0800

    Add sans serif font so my eyes don't burn

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-import App from './App';
-import React from 'react';
-import { StaticRouter } from 'react-router-dom';
-import express from 'express';
-import { renderToString } from 'react-dom/server';
+import App from "./App";
+import React from "react";
+import { StaticRouter } from "react-router-dom";
+import express from "express";
+import { renderToString } from "react-dom/server";
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
 const server = express();
 server
-  .disable('x-powered-by')
+  .disable("x-powered-by")
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get('/*', (req, res) => {
+  .get("/*", (req, res) => {
     const context = {};
     const markup = renderToString(
       <StaticRouter context={context} location={req.url}>
@@ -27,15 +27,16 @@ server
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charset="utf-8" />
+        <style>body {font-family: Helvetica, Arial, sans-serif}</style>
         <title>Welcome to Razzle</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         ${
           assets.client.css
             ? `<link rel="stylesheet" href="${assets.client.css}">`
-            : ''
+            : ""
         }
         ${
-          process.env.NODE_ENV === 'production'
+          process.env.NODE_ENV === "production"
             ? `<script src="${assets.client.js}" defer></script>`
             : `<script src="${assets.client.js}" defer crossorigin></script>`
         }

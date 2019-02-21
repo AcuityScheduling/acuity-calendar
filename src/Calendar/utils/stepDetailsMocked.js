8e28894e059b3a32f1ae4e38986d417b53eb431b
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-02-21 15:38:25 -0800

    Difference between available and closed

--- src/Calendar/utils/stepDetailsMocked.js
+++ src/Calendar/utils/stepDetailsMocked.js
@@ -5,6 +5,13 @@ const detail1 = {
   end: "2019-02-12 13:30:00"
 };
 
-const stepDetailsMocked = [detail1];
+const detail2 = {
+  id: 2,
+  calendar_id: 6,
+  start: "2019-02-13 04:00:00",
+  end: "2019-02-13 19:11:00"
+};
+
+const stepDetailsMocked = [detail1, detail2];
 
 export default stepDetailsMocked;

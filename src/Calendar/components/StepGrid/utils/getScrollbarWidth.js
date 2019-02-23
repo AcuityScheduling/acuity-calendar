9c373bf7aa720e90a3dee73946d03e7b5cef4772
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-02-22 16:07:12 -0800

    Finally working right?

---
+++ src/Calendar/components/StepGrid/utils/getScrollbarWidth.js
@@ -0,0 +1,32 @@
+const isServer = () => {
+  return !(typeof window !== 'undefined' && window.document);
+};
+
+// https://stackoverflow.com/questions/8079187/how-to-calculate-the-width-of-the-scroll-bar
+const getScrollbarWidth = () => {
+  if (isServer()) return 15;
+  var outer = document.createElement('div');
+  outer.style.visibility = 'hidden';
+  outer.style.width = '100px';
+  outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+
+  document.body.appendChild(outer);
+
+  var widthNoScroll = outer.offsetWidth;
+  // force scrollbars
+  outer.style.overflow = 'scroll';
+
+  // add innerdiv
+  var inner = document.createElement('div');
+  inner.style.width = '100%';
+  outer.appendChild(inner);
+
+  var widthWithScroll = inner.offsetWidth;
+
+  // remove divs
+  outer.parentNode.removeChild(outer);
+
+  return widthNoScroll - widthWithScroll;
+};
+
+export default getScrollbarWidth;

ffaa41ff6255dd65c41b50aa2ad3cace2e82dec5
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-03-01 06:10:11 -0800

    Remove comments

--- src/App.js
+++ src/App.js
@@ -58,18 +58,15 @@ const App = () => {
           onExtendEnd={event => {
             updateEvent(event);
             console.log('EXTEND', event);
-            // console.log('event: ', event);
           }}
           onDragEnd={event => {
             updateEvent(event);
             console.log('DRAG', event);
-            // console.log('event: ', event);
           }}
           // First day of the week - 0 indexed on Sunday - Sunday = 0, Monday = 1
           firstDay={firstDay}
           onSelectEvent={event => {
             console.log('CLICK', event);
-            // console.log('event: ', event);
           }}
           // When clicking and dragging to create a new event on a stepgrid view
           onSelecting={({ start, end }) =>
@@ -78,7 +75,6 @@ const App = () => {
           // A callback fired when a date selection is made
           onSelectSlot={start => {
             console.log('SLOT', start);
-            // console.log('start: ', start);
           }}
           // How many grid lines there are between an hour. 30 means
           // break the hour into 30 minute blocks. 20 means to break it into 20 etc.

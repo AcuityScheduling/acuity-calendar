4050751dfe499b24ce98e9356ed2205c35a9ee21
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-03-01 16:28:45 -0800

    add column id to slot so we know what calnedar we're clikcing on

--- src/App.js
+++ src/App.js
@@ -94,8 +94,9 @@ const App = () => {
             setStepDetails(newStepDetails);
           }}
           // A callback fired when a date selection is made
-          onSelectSlot={start => {
-            console.log('SLOT', start);
+          onSelectSlot={({ time, column }) => {
+            console.log('SLOT', time);
+            console.log('SLOT', column);
           }}
           // How many grid lines there are between an hour. 30 means
           // break the hour into 30 minute blocks. 20 means to break it into 20 etc.

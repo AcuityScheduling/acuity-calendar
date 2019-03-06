b3331fd70d224bb1b08375b750366a9b911054f3
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-03-06 15:28:48 -0800

    Clean up month event styling

--- src/Calendar/components/Views/MonthView/index.js
+++ src/Calendar/components/Views/MonthView/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import get from 'lodash/get';
 import { getMonthGrid, getDayNames } from './utils';

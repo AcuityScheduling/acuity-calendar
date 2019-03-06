ae0d9dd2bb0ed3f6bcafffb80b4f3348b3c37107
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-03-06 15:16:11 -0800

    Display time well on month

--- src/Calendar/components/Views/MonthView/components/MonthEvent.js
+++ src/Calendar/components/Views/MonthView/components/MonthEvent.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import EventWrapper from '../../../EventWrapper';
 import { EVENT_TYPE } from '../../../../types';
 import { makeClass } from '../../../../utils';
+import './MonthEvent.scss';
+
+const getDisplayTime = time => {
+  let timeMinutes = `:${time.format('mm')}`;
+  if (timeMinutes === ':00') {
+    timeMinutes = '';
+  }
+  return `${time.format('h')}${timeMinutes}${time.format('a')}`;
+};
 
 const MonthEvent = ({ event, onSelect, children, ...restProps }) => {
   return (
@@ -12,7 +21,16 @@ const MonthEvent = ({ event, onSelect, children, ...restProps }) => {
       eventClass={makeClass('month__event')}
       {...restProps}
     >
-      {children ? children(event) : <div>LOVE IT</div>}
+      {children ? (
+        children(event)
+      ) : (
+        <Fragment>
+          <span className={makeClass('month__event-time')}>
+            {getDisplayTime(event.start)}
+          </span>
+          <span className={makeClass('month__event-title')}>{event.title}</span>
+        </Fragment>
+      )}
     </EventWrapper>
   );
 };

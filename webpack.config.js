f06a870f96812606628e2a74905a61fbbe5d38b4
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-03-18 16:07:54 -0700

    without react

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,32 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.scss'],
   },
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+    },
+    {
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+    },
+    {
+      'prop-types': {
+        root: 'PropTypes',
+        commonjs2: 'prop-types',
+        commonjs: 'prop-types',
+        amd: 'prop-types',
+      },
+    },
+  ],
   optimization: {
     splitChunks: {
       name: 'vendor',

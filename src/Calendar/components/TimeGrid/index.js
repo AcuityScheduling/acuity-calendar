fa835970063afe91c71063d3273576cb36711e48
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-03-20 09:35:08 -0700

    Remove unused type

--- src/Calendar/components/TimeGrid/index.js
+++ src/Calendar/components/TimeGrid/index.js
@@ -3,12 +3,7 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import { STEP_HEIGHTS, STEP_BORDER_WIDTH } from './constants';
 import { useCalendarSticky, getScrollbarWidth, getTopOffset } from './utils';
-import {
-  MOMENT_TYPE,
-  FIRST_DAY_TYPE,
-  STEP_MINUTES_TYPE,
-  DATE_TYPE,
-} from '../../types';
+import { FIRST_DAY_TYPE, STEP_MINUTES_TYPE, DATE_TYPE } from '../../types';
 import { makeClass } from '../../utils';
 import {
   SELECTED_DATE_DEFAULT,

7e53b57f1682b5c6b30f526d9432855fc8eb0744
Martin Holm Cservenka <mhcservenka@gmail.com>
2021-01-08 15:47:20 -0500

    Add stress test story

--- stories/utils.js
+++ stories/utils.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { MOCKED_EVENTS } from '../src/Calendar/mocks';
+import { addMinutes, format, startOfHour } from 'date-fns';
 
-export const useEvents = () => {
-  const [events, setEvents] = useState(MOCKED_EVENTS);
+export const useEvents = (initialEvents = MOCKED_EVENTS) => {
+  const [events, setEvents] = useState(initialEvents);
   const updateEvent = event => {
     // Find the event key we're going to replace
     let alteredEventKey = false;
@@ -58,4 +59,30 @@ export const useEvents = () => {
     events,
     handlers,
   };
+};
+
+export const eventGenerator = ({
+  numberOfEvents = 100,
+  from = startOfHour(new Date()),
+  length = 10,
+  spacing = 0,
+} = {}) => {
+  const dateFormat = 'yyyy-MM-dd HH:mm:ss';
+  const events = [];
+
+  let lastDate = from;
+  for (let i = 0; i < numberOfEvents; i++) {
+    const newEnd = addMinutes(lastDate, length);
+    events.push({
+      id: i,
+      group_id: 5,
+      start: format(lastDate, dateFormat),
+      end: format(newEnd, dateFormat),
+      title: `Group Meeting ${i + 1}`,
+      column_id: 'America/Los_Angeles',
+    });
+    lastDate = addMinutes(newEnd, spacing);
+  }
+
+  return events;
 };
